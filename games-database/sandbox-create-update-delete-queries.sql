80bf56ab00aa41770afdd4798ad2f6b2b7f69dda
CozyNoel <80797611+CozyDoggo@users.noreply.github.com>
2022-04-27 21:46:33 -0500

    Add databases project
    This adds SQL queries and results to the database project.

---
+++ games-database/sandbox-create-update-delete-queries.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+      sandbox(int16_value, double_value, bool_value)
+VALUES
+     (304, 428.21547, 1)
+
+UPDATE
+    sandbox
+SET
+    int64_value = 3362627748845123,
+	string_value = ':Hello:'
+WHERE
+    id = 53
+
+DELETE FROM sandbox WHERE id = 55
